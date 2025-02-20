b2193810ea76079ed079976cee289a7ff68b57e0
noizy <daniil.dokuchits@gmail.com>
2025-02-20 09:21:23 +0100

    3

--- src/main/java/com/cinema/filmlibrary/FilmLibraryApplication.java
+++ src/main/java/com/cinema/filmlibrary/FilmLibraryApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FilmLibraryApplication {
 
+	/**
+	 * Главный метод, запускающий приложение.
+	 * Инициализирует контекст Spring и запускает приложение.
+	 *
+	 * @param args аргументы командной строки, переданные при старте приложения.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FilmLibraryApplication.class, args);
 	}
